07778dc366aaec13609433a1e3569393069e9812
Yu UiSeon <dmltjs851@gmail.com>
2020-07-03 00:04:15 +0900

    Update NaverAPI secret key

--- bigdata/datacollector/naver_api.py
+++ bigdata/datacollector/naver_api.py
@@ -1,4 +1,15 @@
+import os, json
+
 class NaverAPI:
 
     def __init__(self):
+        DIR_PATH = os.path.dirname(__file__)
+        SECRET_PATH = os.path.join(DIR_PATH, 'secret.json')
+        self.secrets = json.loads(open(SECRET_PATH).read())
+        self.client_id = self.secrets['client_id']
+        self.client_secret = self.secrets['client_secret']
         pass
+
+if __name__ == "__main__":
+    n = NaverAPI()
+    pass
