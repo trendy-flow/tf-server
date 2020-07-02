34c24ec7d309c4ad7b3c0cc59ad6f21caaba352d
Yu UiSeon <dmltjs851@gmail.com>
2020-07-03 00:23:21 +0900

    Update NaverAPI
    - Add search_blog method

--- bigdata/datacollector/naver_api.py
+++ bigdata/datacollector/naver_api.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, requests
 
 class NaverAPI:
 
@@ -8,8 +8,18 @@ class NaverAPI:
         self.secrets = json.loads(open(SECRET_PATH).read())
         self.client_id = self.secrets['client_id']
         self.client_secret = self.secrets['client_secret']
+        self.headers = {'X-Naver-Client-Id' : self.client_id, 'X-Naver-Client-Secret' : self.client_secret, 'Content-Type' : "application/json"}
         pass
+
+    def search_blog(self):
+        url = 'https://openapi.naver.com/v1/search/blog.json'
+        data = dict()
+        data['query'] = '방탄소년단'
+        res = requests.get(url, headers=self.headers, params=data).json()
+        print(res)
+
 
 if __name__ == "__main__":
     n = NaverAPI()
+    n.search_blog()
     pass
