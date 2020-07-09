469e6fb40bbd4c820381a63e0437f15465e9f13d
Yu UiSeon <dmltjs851@gmail.com>
2020-07-10 02:15:12 +0900

    Update naver_api
    - Add mongodb connection

--- bigdata/datacollector/naver_api.py
+++ bigdata/datacollector/naver_api.py
@@ -1,4 +1,7 @@
-import os, json, requests
+import os, json
+import requests
+from pymongo import MongoClient
+
 
 class NaverAPI:
 
@@ -8,6 +11,8 @@ class NaverAPI:
         self.secrets = json.loads(open(SECRET_PATH).read())
         self.client_id = self.secrets['client_id']
         self.client_secret = self.secrets['client_secret']
+        self.db_host = self.secrets['db_host']
+        self.db_port = self.secrets['db_port']
         self.headers = {'X-Naver-Client-Id' : self.client_id, 'X-Naver-Client-Secret' : self.client_secret, 'Content-Type' : "application/json"}
         pass
 
@@ -16,8 +21,10 @@ class NaverAPI:
         data = dict()
         data['query'] = '방탄소년단'
         res = requests.get(url, headers=self.headers, params=data).json()
-        print(res)
-
+        db = MongoClient(self.db_host, self.db_port)
+        blog_db = db['blogdb']
+        post = blog_db['post']
+        post.insert_many(res['items'])
 
 if __name__ == "__main__":
     n = NaverAPI()
