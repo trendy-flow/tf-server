d0776608f14290ac088db4e0d9047cfb5cf40d6b
Yu UiSeon <dmltjs851@gmail.com>
2020-07-18 13:08:45 +0900

    Update naver_api
    - Add get_result func
    - Update search_blog

--- bigdata/datacollector/naver_api.py
+++ bigdata/datacollector/naver_api.py
@@ -2,7 +2,6 @@ import os, json
 import requests
 from pymongo import MongoClient
 
-
 class NaverAPI:
 
     def __init__(self):
@@ -16,17 +15,28 @@ class NaverAPI:
         self.headers = {'X-Naver-Client-Id' : self.client_id, 'X-Naver-Client-Secret' : self.client_secret, 'Content-Type' : "application/json"}
         pass
 
-    def search_blog(self):
+    def search_blog(self, query, last_date):
         url = 'https://openapi.naver.com/v1/search/blog.json'
         data = dict()
-        data['query'] = '방탄소년단'
-        res = requests.get(url, headers=self.headers, params=data).json()
-        db = MongoClient(self.db_host, self.db_port)
-        blog_db = db['tfdb']
-        post = blog_db['blog']
-        post.insert_many(res['items'])
+        data['query'] = query
+        data['sort'] = 'date'
+        data['display'] = 100
+        data['start'] = 1
+        res = self.get_result(url, data, last_date)
+
+    def get_result(self, url, data, last_date):
+        item_res = list()
+        while True:
+            res = requests.get(url, headers=self.headers, params=data).json()
+            for item in res['items']:
+                if item['postdate'] <= last_date:
+                    return item_res
+                else:
+                    item_res.append(item)
+            data['start'] += 1
+        return item_res
 
 if __name__ == "__main__":
     n = NaverAPI()
-    n.search_blog()
+    n.search_blog('프로그래밍', '20200717')
     pass
