9898a19eae6c656ec0e66e21254fb847b2fd59f4
uiseon.yu <uiseon.yu@navercorp.com>
2020-08-15 23:26:16 +0900

    Add flask

---
+++ bigdata/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from datacollector.naver_api import NaverAPI
+from flask_pymongo import PyMongo
+import pandas as pd
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/test'
+mongo = PyMongo(app)
+
+naver_api = NaverAPI()
+
+@app.route('/', methods=['POST'])
+def collect_blog_data():
+    keyword = request.form['keyword']
+    last_date = request.form['last_date']
+
+    res = naver_api.search_blog(keyword, last_date)
+
+    blogs_collection = mongo.db.blogs
+
+    # Check empty list
+    if res['data']:
+        res['data'] = list({v['link']:v for v in res['data']}.values())
+        blogs_collection.insert_many(res['data'])
+
+    for item in res['data']:
+        item['_id'] = str(item['_id'])
+
+    return jsonify(res)
+    
