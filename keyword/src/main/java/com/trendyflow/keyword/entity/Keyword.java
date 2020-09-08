5a03be5d9b9743af2c90950c0af472d63e019527
dmltjs851 <dmltjs851@gmail.com>
2020-09-09 02:24:15 +0900

    Update keyword entity class

--- keyword/src/main/java/com/trendyflow/keyword/entity/Keyword.java
+++ keyword/src/main/java/com/trendyflow/keyword/entity/Keyword.java
@@ -2,27 +2,31 @@ package com.trendyflow.keyword.entity;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-import javax.persistence.Entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
+@NoArgsConstructor
+@ToString
 @RedisHash("keyword")
 public class Keyword implements Serializable {
     @Id
-    private String id;
     private String keyword;
     private LocalDateTime refreshTime;
+    private String analysisData;
 
     @Builder
-    public Keyword(String id, String keyword, LocalDateTime refreshTime) {
-        this.id = id;
+    public Keyword(String keyword, LocalDateTime refreshTime) {
         this.keyword = keyword;
         this.refreshTime = refreshTime;
     }
 
+    public void setAnalysisData(String analysisData) {
+        this.analysisData = analysisData;
+    }
 }
